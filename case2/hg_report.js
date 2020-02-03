f5a19e2f229879f77171006f017f8ad75b488559
Beckham <ble335west-mec.org>
2020-02-03 15:27:40 -0700

    Case 2 step 5

---
+++ case2/hg_report.js
@@ -0,0 +1,18 @@
+"use strict";
+
+/*
+   New Perspectives on HTML5 and CSS3, 7th Edition
+   Tutorial 10
+   Case Problem 2
+
+   Author: Beckham Le
+   Date:   2/4/20
+   
+   Filename: hg_report.js
+	
+*/
+
+var gameReport = "<h1>" + itemTitle + "</h1>";
+gameReport += "<h2>By:" + itemManufacturer + "</h2>";
+gameReport += "<img src='hg_" + itemID + ".png' alt'" + itemID + "'" + "id= 'gameImg' />";
+gameReport += "<table>";
